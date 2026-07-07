18eccab288b6899ebd45189a45cb8174ae845d6b
RLKsBR <ruanlukasc599@gmail.com>
2026-07-07 19:14:03 -0300

    Adiciona script do Google Analytics

---
+++ assets/js/analytics.js
@@ -0,0 +1,6 @@
+const ARQUIVO_VERMELHO_GA_ID = 'G-TG69DXZH3R';
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', ARQUIVO_VERMELHO_GA_ID);
