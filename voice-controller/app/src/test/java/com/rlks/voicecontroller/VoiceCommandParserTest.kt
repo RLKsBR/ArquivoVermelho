e39c48c7a1d1172dafb476387870578bde599d69
RLKsBR <ruanlukasc599@gmail.com>
2026-08-24 20:55:08 -0300

    Voice Controller: test collapsed English coordinates

--- voice-controller/app/src/test/java/com/rlks/voicecontroller/VoiceCommandParserTest.kt
+++ voice-controller/app/src/test/java/com/rlks/voicecontroller/VoiceCommandParserTest.kt
@@ -19,6 +19,20 @@ class VoiceCommandParserTest {
     fun parsesCompactCoordinateMove() {
         assertEquals(VoiceCommand.ChessMove("d2", "d4"), VoiceCommandParser.parse("D2D4"))
         assertEquals(VoiceCommand.ChessMove("b1", "c3"), VoiceCommandParser.parse("B1C3"))
+        assertEquals(VoiceCommand.ChessMove("b8", "c6"), VoiceCommandParser.parse("B8 C6"))
+    }
+
+    @Test
+    fun parsesCollapsedSameFileCoordinates() {
+        assertEquals(VoiceCommand.ChessMove("e7", "e5"), VoiceCommandParser.parse("E75"))
+        assertEquals(VoiceCommand.ChessMove("d7", "d5"), VoiceCommandParser.parse("D 75"))
+        assertEquals(VoiceCommand.ChessMove("e7", "e5"), VoiceCommandParser.parse("E seven five"))
+    }
+
+    @Test
+    fun parsesEnglishLetterMisrecognition() {
+        assertEquals(VoiceCommand.ChessMove("e7", "e5"), VoiceCommandParser.parse("he seven he five"))
+        assertEquals(VoiceCommand.ChessMove("d7", "d5"), VoiceCommandParser.parse("the seven the five"))
     }
 
     @Test
